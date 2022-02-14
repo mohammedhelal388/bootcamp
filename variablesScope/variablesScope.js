3219fc1a391ae6da686351165c4e68d9ecb8049e
mohammed helal <mohammedhelal388@gmail.com>
2022-02-14 17:03:18 +0300

    new files and depensencies

---
+++ variablesScope/variablesScope.js
@@ -0,0 +1,36 @@
+// function food()
+// {
+//     var tuna =1
+//     let meat =2
+
+    
+
+    
+// }
+// food();
+//  //console.log(meat)
+//  console.log(tuna)
+
+
+// if(true){
+
+
+
+// var tuna 
+// //let meat =2
+// }
+// console.log(tuna)
+//console.log(meat)
+
+
+// how to make an object of elements
+var obj ={
+    num :'moham',
+    num2:'helal',
+    name:'de',
+    getName:function()
+    {
+        console.log(this.num+ "  " + this.num2 + " "+ this.name);
+    }
+}
+obj.getName();
